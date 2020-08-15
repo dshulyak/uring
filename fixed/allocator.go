f767ebb7b9594747e08f28893df2dbcf36c54191
dmitry <yashulyak@gmail.com>
2020-08-15 19:27:20 +0300

    Don't keep iovec offheapo

--- fixed/allocator.go
+++ fixed/allocator.go
@@ -22,8 +22,6 @@ type allocator struct {
 	max        int // max number of buffers
 	bufferSize int // requested size of the buffer
 
-	buffers int // start of the buffers region in the allocated mem
-
 	// mem is splitted in two parts
 	// header - list of iovec structs.
 	// starts at mem[0]. current length is iovecSz*allocated
@@ -36,18 +34,14 @@ type allocator struct {
 func (a *allocator) init() error {
 	prot := syscall.PROT_READ | syscall.PROT_WRITE
 	flags := syscall.MAP_ANON | syscall.MAP_PRIVATE
-	header := iovecSize
-	size := a.bufferSize*a.max + header
+	size := a.bufferSize * a.max
 	mem, err := syscall.Mmap(-1, 0, size, prot, flags)
 	if err != nil {
 		return err
 	}
-	a.buffers = header
 	a.mem = mem
-	iovec := (*syscall.Iovec)(unsafe.Pointer(&a.mem[0]))
-	iovec.Base = &mem[header]
-	iovec.Len = uint64(size - header)
-	return a.reg.RegisterBuffers(unsafe.Pointer(&a.mem[0]), 1)
+	iovec := syscall.Iovec{Base: &mem[0], Len: uint64(size)}
+	return a.reg.RegisterBuffers(unsafe.Pointer(&iovec), 1)
 }
 
 func (a *allocator) close() error {
@@ -55,7 +49,6 @@ func (a *allocator) close() error {
 }
 
 func (a *allocator) bufAt(pos int) []byte {
-	start := a.buffers + pos*a.bufferSize
-	buf := a.mem[start : start+a.bufferSize]
-	return buf
+	start := pos * a.bufferSize
+	return a.mem[start : start+a.bufferSize]
 }
