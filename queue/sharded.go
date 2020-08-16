bf63505024d162bd2a57e94fa4692152cdf028a9
dmitry <yashulyak@gmail.com>
2020-08-16 10:33:40 +0300

    Work can be assigned in rr to shards in ShardedQueue
    This is useful with O_SYNC flag

--- queue/sharded.go
+++ queue/sharded.go
@@ -2,11 +2,42 @@ package queue
 
 import (
 	"sync"
+	"sync/atomic"
 	"syscall"
 	"unsafe"
 
 	"github.com/dshulyak/uring"
 )
+
+const (
+	shardedSharedWQ uint64 = 1 << iota
+	shardedThreadID
+	shardedRoundRobin
+)
+
+func defaultShardedOpts() []OptShardedQueue {
+	return []OptShardedQueue{OptSharedWorkers, OptThreadID}
+}
+
+// OptShardedQueue ...
+type OptShardedQueue func(*ShardedQueue)
+
+// OptSharedWorkers will make all shards to reuse workers from the first pool.
+func OptSharedWorkers(q *ShardedQueue) {
+	q.flags |= shardedSharedWQ
+}
+
+// OptThreadID will assign a queue based on goroutine thread ID.
+// Mutually exclusive with OptRoundRobin.
+func OptThreadID(q *ShardedQueue) {
+	q.flags |= shardedThreadID
+}
+
+// OptRoundRobin will assign a queue in round robin.
+// Mutually exclusive with OptThreadID.
+func OptRoundRobin(q *ShardedQueue) {
+	q.flags |= shardedRoundRobin
+}
 
 // ShardedQueue distributes submissions over several shards, each shard running
 // on its own ring. Completions are reaped using epoll on eventfd of the every ring.
@@ -14,25 +45,39 @@ import (
 // regualr Queue, but with submissions that spend more time in kernel - regualr Queue
 // can be as twice as fast as this one.
 type ShardedQueue struct {
+	flags uint64
+
 	shards    []int32
+	n         uint64
 	byEventfd map[int32]*Queue
 	poll      *poll
+
+	// order is used only if queue operates in round robin mode
+	order uint64
 
 	wg sync.WaitGroup
 }
 
 // SetupSharded setups requested number of shards, with shared kernel worker pool.
-func SetupSharded(shards, size uint, params *uring.IOUringParams) (*ShardedQueue, error) {
+func SetupSharded(shards, size uint, params *uring.IOUringParams, opts ...OptShardedQueue) (*ShardedQueue, error) {
+	if len(opts) == 0 {
+		opts = defaultShardedOpts()
+	}
 	var (
+		q          = &ShardedQueue{}
 		queues     = make([]*Queue, shards)
 		paramsCopy uring.IOUringParams
 	)
+	for _, opt := range opts {
+		opt(q)
+	}
+
 	if params != nil {
 		paramsCopy = *params
 	}
 	for i := range queues {
 		use := paramsCopy
-		if i > 0 {
+		if q.flags&shardedSharedWQ > 0 && i > 0 {
 			use.Flags |= uring.IORING_SETUP_ATTACH_WQ
 			use.WQFd = uint32(queues[0].Ring().Fd())
 		}
@@ -42,19 +87,13 @@ func SetupSharded(shards, size uint, params *uring.IOUringParams) (*ShardedQueue
 		}
 		queues[i] = newQueue(ring)
 	}
-	return NewSharded(queues), nil
-}
 
-// NewSharded setups infra required for sharded queue (registers eventds, create epoll instance)
-// and returns the pointer to the new instance of the sharded queue.
-// TODO consider returning errors, instead of panicking
-func NewSharded(queues []*Queue) *ShardedQueue {
 	byEventfd := make(map[int32]*Queue, len(queues))
 	pl, err := newPoll(len(queues))
 	if err != nil {
 		panic(err)
 	}
-	shards := make([]int32, len(queues))
+	shardsList := make([]int32, len(queues))
 	for i, qu := range queues {
 		ring := qu.Ring()
 		for {
@@ -66,20 +105,19 @@ func NewSharded(queues []*Queue) *ShardedQueue {
 			}
 			break
 		}
-		shards[i] = int32(ring.Eventfd())
+		shardsList[i] = int32(ring.Eventfd())
 		if err := pl.addRead(int32(ring.Eventfd())); err != nil {
 			panic(err)
 		}
 		byEventfd[int32(ring.Eventfd())] = qu
 	}
-	q := &ShardedQueue{
-		shards:    shards,
-		byEventfd: byEventfd,
-		poll:      pl,
-	}
+	q.shards = shardsList
+	q.n = uint64(shards)
+	q.byEventfd = byEventfd
+	q.poll = pl
 	q.wg.Add(1)
 	go q.completionLoop()
-	return q
+	return q, nil
 }
 
 func (q *ShardedQueue) completionLoop() {
@@ -105,8 +143,15 @@ func (q *ShardedQueue) getQueue() *Queue {
 	if len(q.shards) == 1 {
 		return q.byEventfd[q.shards[0]]
 	}
-	tid := syscall.Gettid()
-	shard := tid % len(q.shards)
+	var tid uint64
+	if q.flags&shardedThreadID > 0 {
+		tid = uint64(syscall.Gettid())
+	} else if q.flags&shardedRoundRobin > 0 {
+		tid = atomic.AddUint64(&q.order, 1)
+	} else {
+		panic("one of OptThreadID or OptRoundRobin must be set")
+	}
+	shard := tid % q.n
 	return q.byEventfd[q.shards[shard]]
 }
 
